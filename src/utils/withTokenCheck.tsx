18b05b8094db4c5253f83f2776a3ca48503ed36f
Mustafa METE <mustafa_mete1999@hotmail.com>
2024-11-15 12:51:22 +0300

    eslint-bug-fix

--- src/utils/withTokenCheck.tsx
+++ src/utils/withTokenCheck.tsx
@@ -2,13 +2,16 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { checkTokenValidity } from "octopus_task/store/slices/authSlice";
 import { AppDispatch } from "octopus_task/store";
+import React from "react";
 
-const withTokenCheck = (WrappedComponent: unknown) => {
+const withTokenCheck = <P extends object>(
+  WrappedComponent: React.ComponentType<P>
+) => {
   // eslint-disable-next-line react/display-name
-  return (props: unknown) => {
+  return (props: P) => {
     const dispatch = useDispatch<AppDispatch>();
 
-    // 1 dakikada bir istek atacak şekilde interval yazıyoruz ve düzenli olarka token geçerliliğini kontrol ediyoruz.
+    // 1 dakikada bir istek atacak şekilde interval yazıyoruz ve düzenli olarak token geçerliliğini kontrol ediyoruz.
     useEffect(() => {
       const interval = setInterval(() => {
         dispatch(checkTokenValidity());
